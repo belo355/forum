ad6dc97fa5e2b1a15419ab1efc12745d057c4cd9
belo355 <edilson.belo@live.com>
2020-08-21 02:01:20 -0300

    add dto

--- src/main/java/br/com/forumCaellum/controller/TopicosController.java
+++ src/main/java/br/com/forumCaellum/controller/TopicosController.java
@@ -3,21 +3,19 @@ package br.com.forumCaellum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forumCaellum.model.Curso;
 import br.com.alura.forumCaellum.model.Topico;
+import br.com.forumCaellum.controller.dto.TopicoDto;
 
-@Controller
+@RestController
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 		Topico topico = new Topico("Duvida","Duvida com spring", new Curso("Spring","Programacao")); 
-		return Arrays.asList(topico, topico, topico, topico); 
-	}
-	
+		return TopicoDto.convert(Arrays.asList(topico, topico, topico, topico)); 
+	}	
 }
