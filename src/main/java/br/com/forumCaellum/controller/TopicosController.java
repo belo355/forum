45d2b8413c25309761eaf63637af44b2154fa0d8
belo355 <edilson.belo@live.com>
2020-09-02 12:37:11 -0300

    add module cache

--- src/main/java/br/com/forumCaellum/controller/TopicosController.java
+++ src/main/java/br/com/forumCaellum/controller/TopicosController.java
@@ -1,12 +1,17 @@
 package br.com.forumCaellum.controller;
 
-import java.net.URI;
-import java.util.List;
+import java.net.URI; 
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -37,18 +43,25 @@ public class TopicosController {
 	private CursoRepository cursoRepository; 
 
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	@Cacheable(value = "listaTopicos")
+	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso, 
+//				@RequestParam(required=false) int pag, @RequestParam(required=false) int qtd, @RequestParam(required=false) String sort) {
+				Pageable paginacao){
+		
+//		Pageable paginacao = PageRequest.of(pag, qtd, Direction.ASC, sort); 
+		
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.convert(topicos);
 		}else {
-			List<Topico> topicosByCurso = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicosByCurso = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.convert(topicosByCurso);
 		}
 	}
 	
 	@PostMapping
 	@Transactional
+	@CacheEvict(value="listaTopicos",allEntries = true)
 	public ResponseEntity<TopicoDto> register(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.convert(cursoRepository); 
 		topicoRepository.save(topico); 
@@ -58,6 +71,7 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
+	@CacheEvict(value="listaTopicos",allEntries = true)
 	public ResponseEntity<DetalheTopicoDto> detalhar(@PathVariable Long id) {
 		Optional<Topico> topico = topicoRepository.findById(id);
 		if (topico.isPresent()) {
@@ -68,6 +82,7 @@ public class TopicosController {
 	
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaTopicos",allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody AtualizaTopicoForm form){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -80,6 +95,7 @@ public class TopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaTopicos",allEntries = true)
 	public ResponseEntity<?> delete(@PathVariable Long id){ 
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
--- src/main/java/br/com/forumCaellum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/forumCaellum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.forumCaellum.model.Topico;
 
 public class TopicoDto {
@@ -40,8 +42,8 @@ public class TopicoDto {
 		return curso;
 	}
 
-	public static List<TopicoDto> convert(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList()); 
+	public static Page<TopicoDto> convert(Page<Topico> topicos) {
+		return topicos.map(TopicoDto::new); 
 	} 
 
 }
