32340cfda7ba00e13e98979427c037b3ff47a756
belo355 <edilson.belo@live.com>
2021-01-30 14:16:09 -0300

    refactor

--- src/main/java/br/com/forumCaellum/config/ErrorHandler.java
+++ src/main/java/br/com/forumCaellum/config/ErrorHandler.java
@@ -32,7 +32,5 @@ public class ErrorHandler {
 		});
 		
 		return dto; 
-	}	
-	
-
+	}
 }
--- src/main/java/br/com/forumCaellum/controller/TopicosController.java
+++ src/main/java/br/com/forumCaellum/controller/TopicosController.java
@@ -10,6 +10,9 @@ import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import br.com.forumCaellum.controller.form.UpdateTopicForm;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -31,87 +34,90 @@ import br.com.forumCaellum.repository.TopicoRepository;
  *
  * @author Edilson Belo
  */
+@Slf4j
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
 
-	@Autowired
-	private TopicoRepository topicoRepository;
-	
-	@Autowired
-	private CursoRepository cursoRepository; 
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-	@GetMapping
-	@Cacheable(value = "listaTopicos")
-	public Page<TopicoDto> findAllTopics(@RequestParam(required=false) String nomeCurso, Pageable paginacao){
+    @Autowired
+    private CursoRepository cursoRepository;
+
+	private static Logger log = LoggerFactory.getLogger(TopicosController.class);
+
+    @GetMapping
+    @Cacheable(value = "listaTopicos")
+    public Page<TopicoDto> findAll(@RequestParam(required = true) String nomeCurso, Pageable paginacao) {
 //		@RequestParam(required=false) int pag, @RequestParam(required=false) int qtd, @RequestParam(required=false) String sort) {
 //		Pageable paginacao = PageRequest.of(pag, qtd, Direction.ASC, sort); 
-		
-		if (nomeCurso.isEmpty() || nomeCurso == null) {
-			Page<Topico> topicos = topicoRepository.findAll(paginacao);
-			return TopicoDto.convert(topicos);
-		}else {
-			Page<Topico> topicosByCurso = topicoRepository.findByCursoNome(nomeCurso, paginacao);
-			return TopicoDto.convert(topicosByCurso);
-		}
-	}
-	
-	@PostMapping
-	@Transactional
-	@CacheEvict(value="listaTopicos",allEntries = true)
-	public ResponseEntity<TopicoDto> save(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) throws IllegalArgumentException {
-		Topico topico = form.convert(cursoRepository);
-		topicoRepository.save(topico);
-		URI uri = uriBuilder.path("/topicos/{}").buildAndExpand(topico.getId()).toUri();
-		return ResponseEntity.created(uri).body(new TopicoDto(topico));
-	}
-	
-	@GetMapping("/{id}")
-	@CacheEvict(value="listaTopicos",allEntries = true)
-	public ResponseEntity<DetalheTopicoDto> findDetails(@PathVariable Long id) {
-		Optional<Topico> topico = topicoRepository.findById(id);
-		if (topico.isPresent()) {
-			return ResponseEntity.ok(new DetalheTopicoDto(topico.get()));
-		}
-		return ResponseEntity.notFound().build(); 
-	}
 
-	@GetMapping("/dates")
-	@CacheEvict(value="listaTopicos",allEntries = true)
-	public ResponseEntity<DetalheTopicoDto> findTopicsBetweenDates(@PathVariable LocalDateTime dateInital, LocalDateTime dateFinal) {
-		try {
-			List<Topico> topics = topicoRepository.findAll();
-			List<Topico> topicsFinal = topics.stream()
-					.filter(topic -> topic.getDataCriacao().isAfter(dateInital))
-					.collect(Collectors.toList());
+        try {
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
+            return TopicoDto.convert(topicos);
+        } catch (IllegalArgumentException e ) {
+            log.info(e.getMessage());
+        }
+        return null;
+    }
 
-			return ResponseEntity.ok(new DetalheTopicoDto(topicsFinal));
-		}catch (EntityNotFoundException e ){
-			return ResponseEntity.notFound().build();
-		}
-	}
-	
-	@PutMapping("/{id}")
-	@Transactional
-	@CacheEvict(value="listaTopicos",allEntries = true)
-	public ResponseEntity<TopicoDto> update(@PathVariable Long id, @RequestBody UpdateTopicForm form){
-		Optional<Topico> optional = topicoRepository.findById(id);
-		if (optional.isPresent()) {
-			Topico topico = form.atualizar(id, topicoRepository);
-			return ResponseEntity.ok(new TopicoDto(topico));
-		}
-		return ResponseEntity.notFound().build(); 	
-	}
-	
-	@DeleteMapping("/{id}")
-	@Transactional
-	@CacheEvict(value="listaTopicos",allEntries = true)
-	public ResponseEntity<?> delete(@PathVariable Long id){
-		Optional<Topico> optional = topicoRepository.findById(id);
-		if (optional.isPresent()) {
-			topicoRepository.deleteById(id); 
-			return ResponseEntity.ok().build();
-		}
-		return ResponseEntity.notFound().build();
-	}
+    @PostMapping
+    @Transactional
+    @CacheEvict(value = "listaTopicos", allEntries = true)
+    public ResponseEntity<TopicoDto> save(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) throws IllegalArgumentException {
+        Topico topico = form.convert(cursoRepository);
+        topicoRepository.save(topico);
+        URI uri = uriBuilder.path("/topicos/{}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
+    @GetMapping("/{id}")
+    @CacheEvict(value = "listaTopicos", allEntries = true)
+    public ResponseEntity<DetalheTopicoDto> findDetails(@PathVariable Long id) {
+        Optional<Topico> topico = topicoRepository.findById(id);
+        if (topico.isPresent()) {
+            return ResponseEntity.ok(new DetalheTopicoDto(topico.get()));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/dates")
+    @CacheEvict(value = "listaTopicos", allEntries = true)
+    public ResponseEntity<DetalheTopicoDto> findBetweenDates(@PathVariable LocalDateTime dateInital, LocalDateTime dateFinal) {
+        try {
+            List<Topico> topics = topicoRepository.findAll();
+            List<Topico> topicsFinal = topics.stream()
+                    .filter(topic -> topic.getDataCriacao().isAfter(dateInital))
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(new DetalheTopicoDto(topicsFinal));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "listaTopicos", allEntries = true)
+    public ResponseEntity<TopicoDto> update(@PathVariable Long id, @RequestBody UpdateTopicForm form) {
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()) {
+            Topico topico = form.atualizar(id, topicoRepository);
+            return ResponseEntity.ok(new TopicoDto(topico));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "listaTopicos", allEntries = true)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()) {
+            topicoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
